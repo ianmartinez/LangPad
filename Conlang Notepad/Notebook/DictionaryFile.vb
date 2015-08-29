92bde1fd9652ef1b2b406d74ef88e08d1e9a93e3
ianmartinez <ianmartinez97@live.com>
2015-08-29 17:01:37 -0500

    Finish dictionary file

--- Conlang Notepad/Notebook/DictionaryFile.vb
+++ Conlang Notepad/Notebook/DictionaryFile.vb
@@ -53,17 +53,24 @@ Module DictionaryFileAccess
             DictionaryString.Add(New ZiaLine(LineType.KeyValue, "Word " & i, Value))
         Next
 
-        'Write to disk
         System.IO.File.WriteAllText(FilePath, ZiaFile.Write(DictionaryString))
     End Sub
 
     Public Function Open(ByVal FilePath As String) As DictionaryFile
         On Error Resume Next
         Dim NewDictionary As New DictionaryFile
-
         Dim LineList As Dictionary(Of String, String) = ZiaFile.Read(File.ReadAllText(FilePath))
-        For Each PageTitle As KeyValuePair(Of String, String) In LineList
 
+        For Each CurrentLine As KeyValuePair(Of String, String) In LineList
+            If Not CurrentLine.Key.StartsWith("Word") Then Continue For
+            Dim NewWord As New DictionaryWord
+            NewWord.Word = FromCompatibleString(CurrentLine.Value.Split("|").GetValue(0))
+            NewWord.Pronunciation = FromCompatibleString(CurrentLine.Value.Split("|").GetValue(1))
+            NewWord.Definition = FromCompatibleString(CurrentLine.Value.Split("|").GetValue(2))
+            NewWord.Notes = FromCompatibleString(CurrentLine.Value.Split("|").GetValue(3))
+            NewWord.Marked = CurrentLine.Value.Split("|").GetValue(4)
+
+            NewDictionary.Words.Add(NewWord)
         Next
 
         Return NewDictionary
--- Conlang Notepad/WordCard.vb
+++
@@ -1,3 +0,0 @@
-﻿Public Class WordCard
-
-End Class
