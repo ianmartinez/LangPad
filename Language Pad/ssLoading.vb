d9b3350019826376f2060130c7fb8aadff299529
Ian Martinez <ianmartinez97@live.com>
2017-10-15 00:22:43 -0500

    Splash screen now matches theme

--- Language Pad/ssLoading.vb
+++ Language Pad/ssLoading.vb
@@ -1,5 +1,6 @@
 ﻿Public NotInheritable Class ssLoading
     Private Sub ssLoading_Load(sender As Object, e As EventArgs) Handles Me.Load
         lblVersion.Text = "Version " + SplashVersion
+        If My.Settings.Theme = "Snow" Then BackColor = Color.White
     End Sub
 End Class
