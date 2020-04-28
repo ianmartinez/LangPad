e8b4a7d2242679019182a73690b9629e7341a7b1
Ian Martinez <ianmartinez97@live.com>
2020-04-28 11:55:57 -0500

    Add autoupdater update() function

--- src/LangPad/AutoUpdate.vb
+++
@@ -1,7 +0,0 @@
-﻿Imports AutoUpdaterDotNET
-
-Module AutoUpdate
-    Public Sub Update()
-        AutoUpdater.Start("https://raw.githubusercontent.com/ianmartinez/LangPad/master/update/update.xml")
-    End Sub
-End Module
---
+++ src/LangPad/AutoUpdater.vb
@@ -0,0 +1,11 @@
+﻿Imports AutoUpdaterDotNET
+
+Module AutoUpdater
+    Public Sub Update()
+        Dim ScreenSize = My.Computer.Screen.Bounds.Size
+        Dim OldSize = AutoUpdaterDotNET.AutoUpdater.UpdateFormSize
+        AutoUpdaterDotNET.AutoUpdater.UpdateFormSize = New Size(ScreenSize.Width / 2, If(OldSize Is Nothing, ScreenSize.Height / 2, OldSize.Value.Height))
+
+        AutoUpdaterDotNET.AutoUpdater.Start("https://raw.githubusercontent.com/ianmartinez/LangPad/master/update/update.xml")
+    End Sub
+End Module
