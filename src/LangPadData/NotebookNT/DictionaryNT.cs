0f6d46eef93e05a980dc2d6c65c0e4b83b4c13d9
Ian Martinez <ianmartinez97@live.com>
2020-05-01 15:11:19 -0500

    Move WordNT to its own class file

--- src/LangPadData/NotebookNT/DictionaryNT.cs
+++ src/LangPadData/NotebookNT/DictionaryNT.cs
@@ -87,22 +87,4 @@ namespace LangPadData.NotebookNT
             }
         }
     }
-
-    public class WordNT : IComparable<WordNT>, IComparer<WordNT>
-    {
-        public string Word { get; set; } = "";
-        public string Pronunciation { get; set; } = "";
-        public string Definition { get; set; } = "";
-        public string Notes { get; set; } = "";
-
-        public int Compare(WordNT x, WordNT y)
-        {
-            return x.CompareTo(y);
-        }
-
-        public int CompareTo(WordNT other)
-        {
-            return Word.CompareTo(other.Word);
-        }
-    }
 }
---
+++ src/LangPadData/NotebookNT/WordNT.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LangPadData.NotebookNT
+{
+    public class WordNT : IComparable<WordNT>, IComparer<WordNT>
+    {
+        public string Word { get; set; } = "";
+        public string Pronunciation { get; set; } = "";
+        public string Definition { get; set; } = "";
+        public string Notes { get; set; } = "";
+
+        public int Compare(WordNT x, WordNT y)
+        {
+            return x.CompareTo(y);
+        }
+
+        public int CompareTo(WordNT other)
+        {
+            return Word.CompareTo(other.Word);
+        }
+    }
+}
