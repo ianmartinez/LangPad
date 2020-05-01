d615f137ffadb9b737d8304130fb26e8448a25d6
Ian Martinez <ianmartinez97@live.com>
2020-05-01 16:27:26 -0500

    Document PageNT

--- src/LangPadData/NotebookNT/PageNT.cs
+++ src/LangPadData/NotebookNT/PageNT.cs
@@ -1,8 +1,18 @@
 ﻿namespace LangPadData.NotebookNT
 {
+    /// <summary>
+    /// A page in the NT 1.x-2.x file format (*.nt).
+    /// </summary>
     public class PageNT
     {
+        /// <summary>
+        /// The page's title.
+        /// </summary>
         public string Title { get; set; } = "";
+
+        /// <summary>
+        /// The Rich Text Format data of the page.
+        /// </summary>
         public string Rtf { get; set; } = "";
     }
 }
