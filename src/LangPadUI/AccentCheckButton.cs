fcd3c0ac69ff4a5f19ce78f9c8926f783019da71
Ian Martinez <ianmartinez97@live.com>
2020-05-05 12:52:00 -0500

    Move character button and IpaToolTip to C#

---
+++ src/LangPadUI/AccentCheckButton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace LangPadUI
+{
+    public class AccentCheckButton : CheckBox
+    {
+        private readonly IpaToolTip CharButtonTooltip = new IpaToolTip();
+
+        public AccentCheckButton(string CharName = "", bool MultiLine = true)
+        {
+            Padding = new Padding(0);
+            Font = new Font("Calibri", 14, FontStyle.Regular);
+            AutoSize = true;
+            MinimumSize = new Size(45, 32);
+            Margin = new Padding(1);
+            Appearance = Appearance.Button;
+            TextAlign = ContentAlignment.MiddleCenter;
+            UseCompatibleTextRendering = true;
+            CharButtonTooltip.CharName = MultiLine ? CharName.Replace("/", "\n") : CharName;
+            TextChanged += AccentCheckButton_TextChanged;
+        }
+
+        private void AccentCheckButton_TextChanged(object sender, EventArgs e)
+        {
+            CharButtonTooltip.SetToolTip(this, Text);
+        }
+    }
+}
---
+++ src/LangPadUI/CharacterButton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace LangPadUI
+{
+    public class CharacterButton : Button
+    {
+        private readonly System.ComponentModel.IContainer components;
+        private readonly IpaToolTip CharButtonTooltip = new IpaToolTip();
+
+        /// <summary>
+        /// Create a new button representing a character.
+        /// </summary>
+        /// 
+        /// <param name="CharName">The name of the character.</param>
+        /// <param name="MultiLine">If the description is on multiple lines.</param>
+        public CharacterButton(string CharName = "", bool MultiLine = true)
+        {
+            Padding = new Padding(0);
+            ForeColor = Color.White;
+            BackColor = Color.Transparent;
+            Font = new Font("Calibri", 11, FontStyle.Bold);
+            AutoSize = true;
+            MinimumSize = new Size(45, 45);
+            Margin = new Padding(3, 3, 3, 6);
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            UseCompatibleTextRendering = true;
+            CharButtonTooltip.CharName = MultiLine ? CharName.Replace("/", "\n") : CharName;
+            TextChanged += CharacterButton_TextChanged;
+        }
+
+        private void CharacterButton_TextChanged(object sender, EventArgs e)
+        {
+            CharButtonTooltip.SetToolTip(this, Text);
+        }
+    }
+}
